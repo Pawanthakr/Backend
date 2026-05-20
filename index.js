3be4f9245c32e39f42e044269b636e4beadc03c6
Pawanthakr <pawanthakre8n@gmail.com>
2026-05-20 22:22:20 +0530

    Minor project (with CRUD opr.)

--- index.js
+++ index.js
@@ -11,26 +11,57 @@ app.use(express.static(Path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
     res.render("index");
-})
+});
 
 app.get("/read", async (req, res) => {
-    let users = await userModule.find();
-    res.render("read", { users });
-})
+    let events = await userModule.find();
+    res.render("read", { events });
+});
+app.get("/add", async (req, res) => {
+    res.render("add");
+});
+app.get("/about", async (req, res) => {
+    res.render("about");
+});
+app.get("/contact", async (req, res) => {
+    res.render("contact");
+});
+app.get("/signup", async (req, res) => {
+    res.render("signup");
+});
+app.get("/login", async (req, res) => {
+    res.render("login");
+});
 
+app.get("/edit/:eventid", async (req, res) => {
+    let event = await userModule.findOne({ _id: req.params.eventid });
+    res.render("edit", { event: event });
+});
+
+app.post("/update/:eventid", async (req, res) => {
+    let { eventtitle, location, image, date, time, category, contact, description } = req.body;
+    let event = await userModule.findOneAndUpdate({ _id: req.params.eventid }, { eventtitle, location, image, date, time, category, contact, description }, { new: true });
+    res.redirect("/read");
+});
 
 app.get("/delete/:id", async (req, res) => {
-    let users = await userModule.findOneAndDelete({ _id: req.params.id });
+    let events = await userModule.findOneAndDelete({ _id: req.params.id });
     res.redirect("/read");
 })
 
 app.post("/create", async (req, res) => {
-    let { name, email, image } = req.body;
-    let createduser = await userModule.create({
-        name,
-        email,
-        image
+    let { eventtitle, location, image, date, time, category, contact, description } = req.body;
+    let createdevent = await userModule.create({
+        eventtitle,
+        location,
+        image,
+        date,
+        time,
+        category,
+        contact,
+        description
     });
+    console.log(req.body);
     res.redirect("/read");
 })
 
--- models/usermodel.js
+++ models/usermodel.js
@@ -1,14 +1,20 @@
 // MongoDB-> It is a data base which store data(text,photo,any data)
 // Database(A,B,C)->collection(A->userdata,salesdata,etc)->document(A->userdata->1person,2person,etc)
 
+const { name } = require("ejs");
 const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/mongodbpractice');
 
 const userSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    image: String
+    eventtitle: String,
+    description: String,
+    date: String,
+    time: String,
+    location: String,
+    image: String,
+    category: String,
+    contact: Number
 })
 
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("event", userSchema);
